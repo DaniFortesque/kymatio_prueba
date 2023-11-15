9523c6ac4e2b2a0fa817260423fad279bcf39bed
Daniel Martínez <daniel.martinez@gotrendier.com>
2023-11-15 09:52:34 +0100

    Modificaciones en la API para devolver json

--- kymatio/src/Controller/CustomerController.php
+++ kymatio/src/Controller/CustomerController.php
@@ -11,37 +11,57 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormInterface;
 use OpenApi\Annotations as OA;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Doctrine\DBAL\Exception;
 
 class CustomerController extends AbstractController {
 
     private CustomerRepositoryInterface $repository;
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
 
     public function __construct(
-        CustomerRepositoryInterface $repository
+        CustomerRepositoryInterface $repository,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
     )  {
         $this->repository = $repository;
+        $this->serializer = $serializer;
+        $this->validator = $validator;
     }
 
     /**
      * 
-     * Muestra todos los clientes
+     * Get all customers
      * 
      * @OA\Post(
-     *     path="/index",
+     *     path="/read",
      *     summary="Muestra todos los clientes",
      *     description="Muestra todos los clientes",
-     *     operationId="index",
+     *     operationId="read",
      *     @OA\Response(
      *         response=200,
-     *         description="Elemento eliminado exitosamente"
+     *         description="Clientes obtenidos exitosamente"
+     *     ),
+     *     @OA\Response(
+     *         response=503,
+     *         description="No es posible conectar con la base de datos"
      *     )
      * )
      */
-    public function index(): Response
+    public function read(): Response
     {
-        $customers = $this->repository->getAllCustomers();
+        try {
+            $customers = $this->repository->getAllCustomers();
+        }
+        catch(Exception $e) {
+            return new Response($e->getMessage(), Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+        
+        $jsonData = $this->serializer->serialize($customers, 'json');
 
-        return $this->render('index.html.twig', ['customers' => $customers]);
+        return new Response($jsonData, Response::HTTP_OK, ['Content-Type', 'application/json']);
     }
 
     /**
@@ -56,6 +76,14 @@ class CustomerController extends AbstractController {
      *     @OA\Response(
      *         response=200,
      *         description="Cliente creado exitosamente"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Datos de la petición incorrectos"
+     *     ),
+     *     @OA\Response(
+     *         response=503,
+     *         description="No es posible conectar con la base de datos"
      *     ),
      *     @OA\RequestBody(
      *         description="Input data format",
@@ -90,18 +118,28 @@ class CustomerController extends AbstractController {
      */
     public function create(Request $request): Response
     {
-        $form = $this->formGenerator()
-            ->handleRequest($request);
+        $requestData = json_decode($request->getContent(), true);
 
-        if($request->getContent()){
-            $customer = $form->getData();
+        $newCustomer = new Customer();
+        $newCustomer->setName($requestData['name']);
+        $newCustomer->setAddress($requestData['address']);
+        $newCustomer->setProvince($requestData['province']);
+        $newCustomer->setCif($requestData['cif']);
 
-            $this->repository->save($customer);
+        $errors = $this->validator->validate($newCustomer);
 
-            return $this->redirectToRoute('api.index');
+        if(count($errors) > 0) {
+            return new Response("Hay campos en blanco", Response::HTTP_BAD_REQUEST);
         }
 
-        return $this->render('create.html.twig', ['form' => $form->createView()]);
+        try {
+            $this->repository->save($newCustomer);
+        }
+        catch(Exception $e) {
+            return new Response($e->getMessage(), Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+        
+        return new Response("Cliente creado correctamente", Response::HTTP_OK);
     }
 
         /**
@@ -123,6 +161,14 @@ class CustomerController extends AbstractController {
      *     @OA\Response(
      *         response=200,
      *         description="Informacion del cliente actualizada exitosamente"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Datos de la petición incorrectos"
+     *     ),
+     *     @OA\Response(
+     *         response=503,
+     *         description="No es posible conectar con la base de datos"
      *     ),
      *     @OA\RequestBody(
      *         description="Input data format",
@@ -157,23 +203,81 @@ class CustomerController extends AbstractController {
      */
     public function update(Request $request, int $id): Response
     {
-        $form = $this->formGenerator($id)
-            ->handleRequest($request);
+        try {
+            $requestData = json_decode($request->getContent(), true);
 
-        if($request->getContent()){
-            $customer = $form->getData();
+            $customer = $this->repository->getCustomer($id);
+            $customer->setName($requestData['name']);
+            $customer->setAddress($requestData['address']);
+            $customer->setProvince($requestData['province']);
+            $customer->setCif($requestData['cif']);
 
+            $errors = $this->validator->validate($customer);
+
+            if(count($errors) > 0) {
+                return new Response(json_encode(['message' => 'Hay campos en blanco']), Response::HTTP_BAD_REQUEST);
+            }
+    
             $this->repository->save($customer);
-
-            return $this->redirectToRoute('api.index');
+            
+            return new Response(json_encode(['message' => 'Cliente actualizado correctamente']), Response::HTTP_OK);
         }
-
-        return $this->render('update.html.twig', ['form' => $form->createView()]);
+        catch(Exception $e) {
+            return new Response(json_encode(['message' => $e->getMessage()]), Response::HTTP_SERVICE_UNAVAILABLE);
+        }
     }
 
     /**
      * 
      * Delete an existing customer
+     * 
+     * @OA\Delete(
+     *     path="/delete/{id}",
+     *     summary="Eliminar un elemento por ID",
+     *     description="Elimina un elemento en base a su ID.",
+     *     operationId="delete",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID del elemento a eliminar",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Elemento eliminado exitosamente"
+     *     ),
+     *     @OA\Response(
+     *         response=503,
+     *         description="No es posible conectar con la base de datos"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="El cliente no existe"
+     *     )
+     * )
+     */
+    public function delete(int $id): Response
+    {
+        try {
+            $customer = $this->repository->getCustomer($id);
+        }
+        catch(Exception $e) {
+            return new Response(json_encode(['message' => $e->getMessage()]), Response::HTTP_SERVICE_UNAVAILABLE);
+        }
+
+        if(is_null($customer)) {
+            return new Response(json_encode(['message' => 'El cliente con id '.$id.' no existe']), Response::HTTP_NOT_FOUND);
+        }
+       
+        $this->repository->delete($id);
+        
+        return new Response(json_encode(['message' => 'Cliente borrado correctamente']), Response::HTTP_OK);
+    }
+
+    /**
+     * 
+     * Get a single customer
      * 
      * @OA\Post(
      *     path="/delete/{id}",
@@ -190,32 +294,30 @@ class CustomerController extends AbstractController {
      *     @OA\Response(
      *         response=200,
      *         description="Elemento eliminado exitosamente"
+     *     ),
+     *     @OA\Response(
+     *         response=503,
+     *         description="No es posible conectar con la base de datos"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="El cliente no existe"
      *     )
      * )
      */
-    public function delete(int $id): Response
+    public function getCustomer(int $id): Response
     {
-
-        $this->repository->delete($id);
-
-        return $this->redirectToRoute('api.index');
-    }
-
-    private function formGenerator(int $id = null): FormInterface
-    {
-        $customer = new Customer();
-
-        if($id) {
+        try {
             $customer = $this->repository->getCustomer($id);
         }
+        catch(Exception $e) {
+            return new Response(json_encode(['message' => $e->getMessage()]), Response::HTTP_SERVICE_UNAVAILABLE);
+        }
 
-        return $this->createFormBuilder($customer)
-            ->add('name', TextType::class, array('attr' =>array('class' => 'form-control mb-0', 'required')))
-            ->add('address', TextType::class, array('attr' =>array('class' => 'form-control', 'required')))
-            ->add('province', TextType::class, array('attr' =>array('class' => 'form-control', 'required')))
-            ->add('cif', TextType::class, array('attr' =>array('class' => 'form-control', 'required')))
-            ->add('save', SubmitType::class, array('label' =>'Aceptar','attr' =>array('class'=>'btn btn-primary mt-3')))
-            ->getForm();
+        if(is_null($customer)) {
+            return new Response(json_encode(['message' => 'El cliente con id '.$id.' no existe']), Response::HTTP_NOT_FOUND);
+        }
+
+        return new Response(json_encode($customer->toArray()), Response::HTTP_OK, ['Content-Type', 'application/json']);
     }
-
 }
--- kymatio/src/Entity/Customer.php
+++ kymatio/src/Entity/Customer.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CustomerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
 class Customer
@@ -14,15 +15,19 @@ class Customer
     private int $id;
 
     #[ORM\Column]
+    #[Assert\NotBlank(message: "El nombre no puede estar en blanco")]
     private string $name;
 
     #[ORM\Column]
+    #[Assert\NotBlank(message: "La dirección no puede estar en blanco")]
     private string $address;
 
     #[ORM\Column]
+    #[Assert\NotBlank(message: "La provincia no puede estar en blanco")]
     private string $province;
 
     #[ORM\Column]
+    #[Assert\NotBlank(message: "El CIF no puede estar en blanco")]
     private string $cif;
 
     #[ORM\Column]
@@ -83,6 +88,16 @@ class Customer
     public function setCif(string $cif): void
     {
         $this->cif = $cif;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'address' => $this->address,
+            'province' => $this->province,
+            'cif' => $this->cif
+        ];
     }
 
     private function generateRandomId(int $length = 5): string
